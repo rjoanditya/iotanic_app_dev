9a5cdbe4dd13fa432f9104c38a51197092b2218b
Rizky Joanditya Nur Iman <rizky.joanditya.9@gmail.com>
2023-03-15 01:06:19 +0700

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,5 @@
 const API_KEY_WEATHER = '97b79a012a0c96b063f425bc40e3fa87';
 
 const TOKEN_MAPBOX = 'sk.eyJ1IjoicmpvYW5kaXR5YSIsImEiOiJjbGY4aHkwYWcxcHdvM3NvMWpvN2p1YTBwIn0._8_wr7_oKzSFPTFFk5vRVg';
-const linkStyle = 'mapbox://styles/rjoanditya/clf8iri4w001y01os9qv3z5og';
 const ENDPOINT_MAPBOX =
     'https://api.mapbox.com/styles/v1/rjoanditya/clf8jpb5a000u01rjbqkjww9z/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmpvYW5kaXR5YSIsImEiOiJjbGY4aHE4d2gxdnBiM3lwcTE3dmkzMmlrIn0.Jfp9n4kDcdWciwJojS-3zg';
