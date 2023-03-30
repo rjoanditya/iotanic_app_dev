102b86865a21d9750f0df52d1746809c9cb2b142
Rizky Joanditya Nur Iman <rizky.joanditya.9@gmail.com>
2023-03-31 02:20:21 +0700

    Update signin.dart

--- lib/view/Auth/signin.dart
+++ lib/view/Auth/signin.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'package:flutter/material.dart';
+
 import 'package:iotanic_app_dev/main.dart';
 import 'package:iotanic_app_dev/view/App/index.dart';
 import 'package:provider/provider.dart';
-import 'package:validators/validators.dart';
 
+import 'package:validators/validators.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 // import 'package:iotanic_app_dev/view/App/home.dart';
 // import 'package:hexcolor/hexcolor.dart';
 
@@ -35,11 +38,12 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.25, vertical: screenWidth * 0.2),
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.15, vertical: screenWidth * 0.2),
                 child: themes.getLogo,
               ),
               Form(
@@ -47,125 +51,104 @@ class _SignInState extends State<SignIn> {
                 child: Column(
                   children: [
                     Builder(
-                      builder: (context) => Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 45.0),
-                        child: Column(
-                          children: [
-                            Column(
-                              children: [
-                                Material(
-                                  elevation: 1.5,
-                                  borderRadius: const BorderRadius.all(Radius.circular(50)),
-                                  child: TextFormField(
-                                    validator: (value) {
-                                      if (value == null || !isEmail(value)) {
-                                        return 'Email is not valid!';
-                                      }
-                                    },
-                                    controller: email,
-                                    obscureText: false,
-                                    autofocus: false,
-                                    decoration: InputDecoration(
-                                      border: const OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                                      ),
-                                      hintText: 'Email',
-                                      hintStyle: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),
-                                      fillColor: Theme.of(context).highlightColor,
-                                      filled: true,
-                                      contentPadding: const EdgeInsets.symmetric(
-                                        vertical: 15,
-                                        horizontal: 10,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                      builder: (context) => Container(
+                        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || !isEmail(value)) {
+                              return 'Email is not valid!';
+                            }
+                          },
+                          controller: email,
+                          obscureText: false,
+                          autofocus: false,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(12)),
+                              borderSide: BorderSide.none,
                             ),
-                          ],
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 14,
+                            ),
+                            // hintText: 'Email',
+                            hintStyle: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),
+                            fillColor: Theme.of(context).highlightColor,
+                            filled: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 15,
+                            ),
+                          ),
                         ),
                       ),
                     ),
                     Builder(
-                      builder: (context) => Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 45.0),
-                        child: Column(
-                          children: [
-                            Column(
-                              children: [
-                                Material(
-                                  elevation: 1.5,
-                                  borderRadius: const BorderRadius.all(Radius.circular(50)),
-                                  shadowColor: Theme.of(context).primaryColor,
-                                  child: TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value == "") {
-                                        return 'Password is not valid!';
-                                      }
-                                    },
-                                    controller: password,
-                                    obscureText: !passwordVisible,
-                                    autofocus: false,
-                                    decoration: InputDecoration(
-                                      border: const OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                                      ),
-                                      hintText: 'Password',
-                                      hintStyle: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),
-                                      fillColor: Theme.of(context).highlightColor,
-                                      filled: true,
-                                      contentPadding: const EdgeInsets.symmetric(
-                                        vertical: 15,
-                                        horizontal: 10,
-                                      ),
-                                      suffixIcon: IconButton(
-                                        padding: const EdgeInsets.only(right: 15),
-                                        icon: Icon(
-                                          passwordVisible == true ? Icons.visibility_off : Icons.visibility,
-                                          color: Theme.of(context).primaryColorLight,
-                                        ),
-                                        onPressed: () {},
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
+                      builder: (context) => Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value == "") {
+                              return 'Password is not valid!';
+                            }
+                          },
+                          controller: password,
+                          obscureText: !passwordVisible,
+                          autofocus: false,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(12)),
+                              borderSide: BorderSide.none,
                             ),
-                          ],
+                            labelText: 'Password',
+                            labelStyle: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),
+                            fillColor: Theme.of(context).highlightColor,
+                            filled: true,
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 10,
+                            ),
+                            suffixIcon: IconButton(
+                              padding: const EdgeInsets.only(right: 15),
+                              icon: Icon(
+                                passwordVisible == true ? Icons.visibility_off : Icons.visibility,
+                                color: Theme.of(context).primaryColorLight,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 45.0),
-                child: ButtonTheme(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (BuildContext context) {
-                          return const Index();
-                        }),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.3, vertical: 15),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                        )),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 14,
-                      ),
+              ButtonTheme(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return const Index();
+                      }),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.3, vertical: 17),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      )),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
                     ),
                   ),
                 ),
               ),
-              Padding(padding: const EdgeInsets.fromLTRB(0, 5, 0, 50), child: Text('Lupa Passsword? Atau', style: TextStyle(color: Theme.of(context).primaryColor))),
+              Padding(padding: const EdgeInsets.fromLTRB(0, 10, 0, 50), child: Text('Lupa Passsword? Atau', style: TextStyle(color: Theme.of(context).primaryColor))),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 45.0),
                 child: ButtonTheme(
@@ -198,9 +181,24 @@ class _SignInState extends State<SignIn> {
                   ),
                 ),
               ),
-              Text(
-                'Belum memiliki akun? Daftar',
-                style: TextStyle(color: Theme.of(context).primaryColor),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Belum memiliki akun? ',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Daftar',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ],
           ),
