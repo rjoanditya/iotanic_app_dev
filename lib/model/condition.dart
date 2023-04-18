0b7f3bb215725619921e9a8226fbb035ac3dc737
Rizky Joanditya Nur Iman <rizky.joanditya.9@gmail.com>
2023-04-19 06:23:51 +0700

    Create condition.dart
    this model is used for chart

---
+++ lib/model/condition.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Condition {
+  final String nitrogen;
+  final String phosphorus;
+  final String potassium;
+  final String ph;
+
+  Condition({required this.nitrogen, required this.phosphorus, required this.potassium, required this.ph});
+
+  factory Condition.fromJson(Map<String, dynamic> object) {
+    return Condition(
+      nitrogen: object['nitrogen'] ?? '',
+      phosphorus: object['phosphorus'] ?? '',
+      potassium: object['potassium'] ?? '',
+      ph: object['ph'] ?? '',
+    );
+  }
+  static Future<Condition> fetchRecord(String id) async {
+    String apiURL = "http://34.101.57.46:8080/record/$id";
+
+    var apiResult = await http.get(Uri.parse(apiURL));
+    var jsonObject = json.decode(apiResult.body);
+    var recordData = (jsonObject as Map<String, dynamic>);
+
+    return Condition.fromJson(recordData);
+  }
+
+  @override
+  String toString() => 'Condition($nitrogen,$phosphorus,$potassium,$ph)';
+}
+
+// class UninitializedRecord extends Record {}
