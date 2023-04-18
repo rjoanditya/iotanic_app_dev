829c3f1cb65eb3afb609ec1d64a856c5655b4e9c
Rizky Joanditya Nur Iman <rizky.joanditya.9@gmail.com>
2023-04-19 06:23:02 +0700

    Update splash.dart
    check if the device has connection

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
+import 'package:get/get.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:iotanic_app_dev/main.dart';
 
 import 'package:iotanic_app_dev/view/Auth/signin.dart';
@@ -13,7 +20,22 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  // late StreamSubscription subscription;
+  // late StreamSubscription internetSubscription;
+  // bool hasInternet = false;
   ThemeProvider themes = ThemeProvider();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   subscription = Connectivity().onConnectivityChanged.listen(_showConnectivitySnackBar);
+  // // internetSubscription = InternetConnectionChecker().onStatusChange.listen((status) {
+  // //   final hasInternet = status == InternetConnectionStatus.connected;
+
+  // });
+
+  // setState(() => this.hasInternet = hasInternet);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -45,15 +67,18 @@ class _SplashState extends State<Splash> {
                       elevation: 1,
                       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(builder: (BuildContext context) {
-                          return const SignIn();
-                        }),
-                      );
-                      // ThemeProvider themeProvider = Provider.of<ThemeProvider>(context, listen: false);
-                      // themeProvider.swapTheme();
-                      // print(themes.getLogo);
+                    onPressed: () async {
+                      bool result = await InternetConnectionChecker().hasConnection;
+                      result == false
+                          ? Get.snackbar(
+                              "Ups! Anda Sedang Offline",
+                              'Periksa Koneksi Internet Anda!',
+                              colorText: Theme.of(context).primaryColor,
+                              margin: const EdgeInsets.all(10),
+                            )
+                          : Timer(const Duration(seconds: 1), () {
+                              Get.to(const SignIn());
+                            });
                     },
                     child: const Text(
                       "Get Started",
@@ -68,4 +93,29 @@ class _SplashState extends State<Splash> {
       ),
     );
   }
+
+  // void _showConnectivitySnackBar(ConnectivityResult result) {
+  //   final hasInternet = result != ConnectivityResult.none;
+  //   final message = hasInternet
+  //       ? result == ConnectivityResult.mobile
+  //           ? "you're connected to mobile network"
+  //           : "You're connected to wifi network"
+  //       : "You're not connected";
+
+  //   final color = hasInternet ? Colors.green : Colors.red;
+
+  //   _showSnackBar(context, message, color);
+  // }
+
+  // void _showSnackBar(context, String message, MaterialColor color) {
+  //   Get.snackbar(
+  //     'Alert!',
+  //     message,
+  //     colorText: Theme.of(context).primaryColor,
+  //     borderColor: color,
+  //   );
+  //   Timer(Duration(seconds: 3), () {
+  //     Get.to(SignIn());
+  //   });
+  // }
 }
