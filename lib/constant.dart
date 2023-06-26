0a9b014768a0dc3dcdbab2ec158efd4c150a46b3
Rizky Joanditya Nur Iman <85508134+rjoanditya@users.noreply.github.com>
2023-06-26 16:31:21 +0700

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,13 +1,1 @@
-// ignore_for_file: constant_identifier_names
 
-const API_KEY_WEATHER = '97b79a012a0c96b063f425bc40e3fa87';
-const API_KEY =
-    'zuOoKBIoHzAEpQuH0uN3waw6yRXdpKTJl7Z24mqybtfUEyhw6O5ns5F7fS47ttQgHsSzrjkcrQxEopelU5UmuEH61wZvzllvBHUZpC7YHlq5v6z7BajG3kB47Qj5CHKu8xu6MH7uWMYR7D3zYNSJVDG4pdwN7OruVAAbNbU7Q4aT9KK3qLYAX7e8LtqIjXRj1lLLDqIfmhOQUGcwEQLb87hYXKKfqIYsGblnqQk2ISIcerTQYtjesbL7R9mnrSrF';
-
-const TOKEN_MAPBOX = 'sk.eyJ1IjoicmpvYW5kaXR5YSIsImEiOiJjbGY4aHkwYWcxcHdvM3NvMWpvN2p1YTBwIn0._8_wr7_oKzSFPTFFk5vRVg';
-
-const API_URL = "https://a383-125-166-175-7.ngrok-free.app/";
-// const API_URL = "https://deviotanic-7xjlkrkjga-et.a.run.app/";
-// const API_URL = "http://localhost:5000/";
-const ENDPOINT_MAPBOX =
-    'https://api.mapbox.com/styles/v1/rjoanditya/clf8jpb5a000u01rjbqkjww9z/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmpvYW5kaXR5YSIsImEiOiJjbGY4aHE4d2gxdnBiM3lwcTE3dmkzMmlrIn0.Jfp9n4kDcdWciwJojS-3zg';
